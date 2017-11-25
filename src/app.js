4840cee13bbd20ae9ca4253458eb8956b0f35c6b
ac065455 <anthony123456@gmail.com>
2017-11-25 18:54:06 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/styles.scss';
+
+const template = <p>this is webpack!!</p>
+ReactDOM.render(template, document.getElementById('app'));
