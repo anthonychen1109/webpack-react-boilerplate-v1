f0535263472ee79e5bbc03d65507a64cf5ebceae
anthonychen1109 <chen.anthony.1109@gmail.com>
2017-11-25 22:32:57 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const template = <p>this is webpack!!</p>
